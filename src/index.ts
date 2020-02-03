8e626023ae94a976c587889265eab057601f0fda
João Pereira <joao.pereira@navenio.com>
2020-02-03 16:12:07 +0000

    Export actors from colibri index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
 // TODO export all things :)
+export { Aggregate } from "./Aggregate";
+export { Repository } from "./Repository";
+export { Module } from "./Module";
+export { App } from "./App";
+export { IEvent } from "./IEvent";
+export { IEventHandler } from "./IEventHandler";
+export { ICommand } from "./ICommand";
+export { ICommandHandler } from "./ICommandHandler";
+export { Result } from "./Result";
+export { EventBus } from "./EventBus";
+export { IEventStore } from "./event-store/IEventStore";
+export { Handle } from "decorators/Handle";
+export { Get, Post } from "decorators/http";
+export { ControllerMetadata } from "decorators/ControllerMetadata";
+export { Inject } from "decorators/Inject";
+export { Injectable } from "decorators/Injectable";
+export { CommandHandler } from "decorators/commands";
